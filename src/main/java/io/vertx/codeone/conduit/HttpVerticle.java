ed3dd05911770d7c8765e705a048edc44f8c7518
Joel LaBelle <jlabelle@fairhealth.org>
2020-04-28 00:25:58 -0400

    upgrade all modules to current versions; fix jwtAuth; add log4j2; migrate to JsonObject.decodeValue; migrate to Junit5Tests; add static/404 router

--- src/main/java/io/vertx/codeone/conduit/HttpVerticle.java
+++ src/main/java/io/vertx/codeone/conduit/HttpVerticle.java
@@ -2,92 +2,145 @@ package io.vertx.codeone.conduit;
 
 import io.vertx.codeone.conduit.models.User;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
+import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.net.SocketAddress;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.jwt.JWTAuth;
-import io.vertx.ext.auth.jwt.JWTOptions;
+import io.vertx.ext.auth.jwt.JWTAuthOptions;
+import io.vertx.ext.jwt.JWTOptions;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.JWTAuthHandler;
+import io.vertx.ext.web.handler.StaticHandler;
 
-public class HttpVerticle extends AbstractVerticle{
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-  JWTAuth jwtAuth;
+public class HttpVerticle extends AbstractVerticle {
+    private static final Logger logger = LogManager.getLogger(HttpVerticle.class.getName());
+   
+    private static final String CONTENT_TYPE = "Content-Type";
+    private static final String MEDIA_JSON = "application/json; charset=utf-8";
+    private static final String MEDIA_TEXT = "text/plain";
+    private static final String KEY_PASSWORD = "password";
+    private static final String KEY_TYPE = "type";
+    private static final String KEY_PATH = "path";
 
-  @Override
-  public void start(Future<Void> startFuture) {
+    private JWTAuth jwtAuth;
+
+    @Override
+    public void start(Promise<Void> startFuture) {
 
     // Configure authentication with JWT
-    jwtAuth = JWTAuth.create(vertx, new JsonObject().put("keyStore", new JsonObject()
-      .put("type", "jceks")
-      .put("path", "keystore.jceks")
-      .put("password", "secret")));
+    JsonObject authConfig = new JsonObject().put("keyStore", new JsonObject()
+        .put(KEY_TYPE, "jceks")
+        .put(KEY_PATH, "resources/keystore.jceks")
+        .put(KEY_PASSWORD, "secret"));
+
+    jwtAuth = JWTAuth.create(vertx, new JWTAuthOptions(authConfig));
 
     Router baseRouter = Router.router(vertx);
     Router apiRouter = Router.router(vertx);
 
     baseRouter.route("/").handler(routingContext -> {
-      HttpServerResponse response = routingContext.response();
-      response.putHeader("content-type", "text/plain").end("Hello Vert.x!");
+        logIncoming(routingContext);
+        HttpServerResponse response = routingContext.response();
+        response.putHeader(CONTENT_TYPE, MEDIA_TEXT).end("Hello Vert.x!");
     });
 
     apiRouter.route("/user*").handler(BodyHandler.create());
     apiRouter.post("/users").handler(this::registerUser);
-    apiRouter.get("/user").handler(JWTAuthHandler.create(jwtAuth)).handler(this::getCurrentUser);
+
+    // secure endpoint
+    apiRouter.get("/user-secure").handler(JWTAuthHandler.create(jwtAuth)).handler(this::getCurrentUser);
+
+    apiRouter.get("/user").handler(this::getCurrentUser);
 
     baseRouter.mountSubRouter("/api", apiRouter);
 
-    vertx.createHttpServer()
-      .requestHandler(baseRouter::accept)
-      .listen(8080, result -> {
-        if (result.succeeded()) {
-          startFuture.complete();
-        } else {
-          startFuture.fail(result.cause());
-        }
-      });
-  }
+    // Serve the non private static pages under webroot
+    baseRouter.route("/static/*").handler(StaticHandler.create());
 
-  private void getCurrentUser(RoutingContext routingContext) {
-
-//    public User(String username, String email, String bio, String password, String image, String token) {
-    User jacob = new User("Jacob", "jake@jake.jake", null, "password", null, "jwt.token.here");
-    routingContext.response()
-      .setStatusCode(201)
-      .putHeader("Content-Type", "application/json; charset=utf-8")
-      //.putHeader("Content-Length", String.valueOf(userResult.toString().length()))
-      .end(Json.encodePrettily(jacob.toConduitJson()));
-  }
-
-  private void registerUser(RoutingContext routingContext) {
-
-    JsonObject message = new JsonObject()
-      .put("action", "register-user")
-      .put("user", routingContext.getBodyAsJson().getJsonObject("user"));
-
-    vertx.eventBus().send("persistence-address", message, ar -> {
-
-      if (ar.succeeded()) {
-        User returnedUser = Json.decodeValue(ar.result().body().toString(), User.class);
-        String token = jwtAuth.generateToken(new JsonObject().put("email", returnedUser.getEmail()).put("password", returnedUser.getPassword()), new JWTOptions().setIgnoreExpiration(true));
-        returnedUser.setToken(token);
-        routingContext.response()
-          .setStatusCode(201)
-          .putHeader("Content-Type", "application/json; charset=utf-8")
-          //.putHeader("Content-Length", String.valueOf(userResult.toString().length()))
-          .end(Json.encodePrettily(returnedUser.toConduitJson()));
-      }else{
-        routingContext.response()
-          .setStatusCode(500)
-          .putHeader("Content-Type", "application/json; charset=utf-8")
-          //.putHeader("Content-Length", String.valueOf(userResult.toString().length()))
-          .end(Json.encodePrettily(ar.cause().getMessage()));
-
-      }
+    baseRouter.route("/*").handler(routingContext -> {
+        logIncoming(routingContext);
+        HttpServerResponse response = routingContext.response();
+        response.putHeader(CONTENT_TYPE, MEDIA_TEXT).end("Goodbye Vert.x!");
     });
 
-  }
+    vertx.createHttpServer()
+        .requestHandler(baseRouter)
+        .listen(8080, result -> {
+            if (result.succeeded()) {
+                startFuture.complete();
+            } else {
+                startFuture.fail(result.cause());
+            }
+        });
+    }
+
+    private void getCurrentUser(RoutingContext routingContext) {
+        User jacob = new User("Jacob", "jake@jake.jake", null, KEY_PASSWORD, null, "jwt.token.here");
+
+        routingContext.response()
+            .setStatusCode(201)
+            .putHeader(CONTENT_TYPE, MEDIA_JSON)
+            //.putHeader("Content-Length", String.valueOf(userResult.toString().length()))
+            .end(Json.encodePrettily(jacob.toConduitJson()));
+    }
+   
+    private void registerUser(RoutingContext routingContext) {
+        logIncoming(routingContext);
+        JsonObject message = new JsonObject().put("action", "register-user").put("user",
+                routingContext.getBodyAsJson());
+
+        vertx.eventBus().request("persistence-address", message, ar -> {
+
+            if (ar.succeeded()) {
+                String token = "token-not-set";
+
+                User returnedUser = null;
+                try {
+                    returnedUser = Json.decodeValue(ar.result().body().toString(), User.class);
+                } catch (Exception e) {
+                    logger.info(e);
+                }
+
+                try {
+                    //keytool -genseckey -keystore keystore.jceks -storetype jceks -storepass secret -keyalg HMacSHA512 -keysize 2048 -alias HS512 -keypass secret
+
+                    token = jwtAuth.generateToken(
+                            new JsonObject().put("email", returnedUser.getEmail()).put(KEY_PASSWORD,
+                                    returnedUser.getPassword()),
+                            new JWTOptions().setIgnoreExpiration(true).setAlgorithm("HS512"));
+                } catch (RuntimeException re) {
+                    logger.info(re);
+                }
+
+                logger.info("token: {}", token);
+
+                returnedUser.setToken(token);
+
+                routingContext.response().setStatusCode(201).putHeader(CONTENT_TYPE, MEDIA_JSON)
+                        //.putHeader("Content-Length", String.valueOf(userResult.toString().length()))
+                        .end(Json.encodePrettily(returnedUser.toConduitJson()));
+            } else {
+                routingContext.response().setStatusCode(500).putHeader(CONTENT_TYPE, MEDIA_JSON)
+                        //.putHeader("Content-Length", String.valueOf(userResult.toString().length()))
+                        .end(Json.encodePrettily(ar.cause().getMessage()));
+
+            }
+        });
+
+    }
+    private void logIncoming(RoutingContext routingContext) {
+        HttpServerRequest request = routingContext.request();
+        SocketAddress remoteAddress = request.remoteAddress();
+        String remoteHost = remoteAddress.host();
+        
+        logger.info("{} requesting {}", remoteHost, routingContext.request().absoluteURI().toString());
+    }
 }
--- src/test/java/io/vertx/codeone/conduit/EventBusTest.java
+++ src/test/java/io/vertx/codeone/conduit/EventBusTest.java
@@ -1,5 +1,10 @@
 package io.vertx.codeone.conduit;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import io.vertx.codeone.conduit.models.PersistenceVerticle;
 import io.vertx.codeone.conduit.models.User;
 import io.vertx.core.Vertx;
@@ -8,69 +13,62 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 
 @RunWith(VertxUnitRunner.class)
 public class EventBusTest {
 
-  private Vertx vertx;
+    private Vertx vertx;
 
-  @Before
-  public void setUp(TestContext testContext) {
+    @Before
+    public void setUp(TestContext testContext) {
 
     vertx = Vertx.vertx();
     vertx.deployVerticle(PersistenceVerticle.class.getName(), testContext.asyncAssertSuccess());
 
-  }
+    }
+    @After
+    public void tearDown(TestContext testContext) {
+        vertx.close();
+    }
 
-  @After
-  public void tearDown(TestContext testContext) {
-    vertx.close();
-  }
+    /**
+     * {
+     *     "user" :
+     *     {
+     *      "email" : "jake@jake.jake,
+     *      "token" : null
+     *      "username": "Jacob",
+     *      "bio" : null,
+     *      "image" " null
+     *     }
+     * }
+     * @param testContext
+     */
+    @Test
+    public void testRegisterUserMessage(TestContext testContext) {
 
-  /**
-   * {
-   *     "user" :
-   *     {
-   *      "email" : "jake@jake.jake,
-   *      "token" : null
-   *      "username": "Jacob",
-   *      "bio" : null,
-   *      "image" " null
-   *     }
-   * }
-   * @param testContext
-   */
-  @Test
-  public void testRegisterUserMessage(TestContext testContext) {
+        Async async = testContext.async();
 
-    Async async = testContext.async();
+        JsonObject userToRegister = new JsonObject()
+            .put("email", "jake@jake.jake")
+            .put("username", "Jacob")
+            .put("password", "jakejake");
 
-    JsonObject userToRegister = new JsonObject()
-      .put("email", "jake@jake.jake")
-      .put("username", "Jacob")
-      .put("password", "jakejake");
+        JsonObject message = new JsonObject()
+            .put("action", "register-user")
+            .put("user", userToRegister);
 
-    JsonObject message = new JsonObject()
-      .put("action", "register-user")
-      .put("user", userToRegister);
-
-    vertx.<JsonObject>eventBus().send("persistence-address", message, ar -> {
-      if (ar.succeeded()) {
-        testContext.assertNotNull(ar.result().body());
-        User returnedUser = Json.decodeValue(ar.result().body().toString(), User.class);
-        testContext.assertEquals("jake@jake.jake", returnedUser.getEmail());
-        testContext.assertEquals("Jacob", returnedUser.getUsername());
-        async.complete();
-      }else{
-        testContext.assertTrue(ar.succeeded());
-        async.complete();
-      }
-    });
-  }
-
-
+        vertx.eventBus().request("persistence-address", message, ar -> {
+            if (ar.succeeded()) {
+                testContext.assertNotNull(ar.result().body());
+                User returnedUser = Json.decodeValue(ar.result().body().toString(), User.class);
+                testContext.assertEquals("jake@jake.jake", returnedUser.getEmail());
+                testContext.assertEquals("Jacob", returnedUser.getUsername());
+                async.complete();
+            }else{
+                testContext.assertTrue(ar.succeeded());
+                async.complete();
+            }
+        });
+    }
 }
